97ec2941c34c6c0b49344fa0e70b025a68b0bf17
Stefan Ferstl <st.ferstl@gmail.com>
2015-04-05 11:56:34 +0200

    Extract downloaded JDK and execute java

--- .travis/setup-environment.sh
+++ .travis/setup-environment.sh
@@ -2,4 +2,8 @@
 
 echo "setup environment"
 curl -LOH "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u40-b26/jdk-8u40-linux-x64.tar.gz
+
+tar -zxf jdk-8u40-linux-x64.tar.gz
+jdk1.8.0_40/bin/java -version
+
 ls -al
