75b4a7d55b59d0c2febf56b509270b3e3be850e8
Stefan Ferstl <st.ferstl@gmail.com>
2015-04-05 11:31:14 +0200

    Try downloading JDK from oracle.com

--- .travis/setup-environment.sh
+++ .travis/setup-environment.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 echo "setup environment"
+curl -LOH "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u40-b26/jdk-8u40-linux-x64.tar.gz
+ls -al
