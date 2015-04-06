b245e06c41abd08ddd2e0a3554dd3eae808d2192
Stefan Ferstl <st.ferstl@gmail.com>
2015-04-06 12:27:06 +0200

    Implement installation of Oracle JDK

--- .travis/setup-environment.sh
+++ .travis/setup-environment.sh
@@ -1,9 +1,92 @@
 #!/bin/bash
 
-echo "setup environment"
-curl -LOH "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u40-b26/jdk-8u40-linux-x64.tar.gz
+# ###########################################################################
+# Locations
+# ###########################################################################
+BASEDIR=`cd $(dirname $0); pwd`
+WORKINGDIR=`pwd`
+SCRIPT_NAME=`basename $0`
 
-tar -zxf jdk-8u40-linux-x64.tar.gz
-jdk1.8.0_40/bin/java -version
+# Use TEST_ROOT for local testing
+ROOT_DIR=${HOME}
+[ ! -z ${TEST_ROOT} ] && ROOT_DIR=${TEST_ROOT}
+DOWNLOAD_DIR=${ROOT_DIR}/download
+INSTALL_DIR=${ROOT_DIR}/bin
 
-ls -al
+
+# ###########################################################################
+# Variables
+# ###########################################################################
+ORACLE_DOWNLOAD_COOKIE="Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie"
+
+print_env() {
+  cat << EOF
+Build Environment Setup
+-----------------------
+
+Script                 $SCRIPT_NAME
+Base directory         $BASEDIR
+Working directory      $WORKINGDIR
+Installation Root      $ROOT_DIR
+Download directory     $DOWNLOAD_DIR
+Installation directory $INSTALL_DIR
+
+EOF
+}
+
+init() {
+  echo "Creating directories"
+  mkdir -vp ${DOWNLOAD_DIR}
+  mkdir -vp ${INSTALL_DIR}
+
+  echo "Initializatin complete"
+}
+
+download_and_install_oracle_jdk() {
+  jdk_major_version=$1
+  jdk_minor_version=$2
+  jdk_build_number=$3
+
+  # JDK full version string as it appears in the download URL, e.g. 8u40-b26
+  jdk_full_version_string=${jdk_major_version}u${jdk_minor_version}-b${jdk_build_number}
+
+  # JDK version string as it appears in the archive name
+  jdk_version_string=${jdk_major_version}u${jdk_minor_version}
+
+  # JDK version number as it appears within the archive, e.g. 1.8.0_40
+  archive_version="1.${jdk_major_version}.0_${jdk_minor_version}"
+
+  # Final name of the downloaded file and of the installation folder
+  final_name="oracle-jdk-${archive_version}"
+
+  download_url=http://download.oracle.com/otn-pub/java/jdk/${jdk_full_version_string}/jdk-${jdk_version_string}-linux-x64.tar.gz
+  download_target=${DOWNLOAD_DIR}/oracle-jdk-${jdk_version_string}.tar.gz
+
+  if [ ! -f ${download_target} ]; then
+    echo "Downloading ${final_name}"
+    curl --create-dirs -o ${download_target} -LH "${ORACLE_DOWNLOAD_COOKIE}" "${download_url}"
+  else
+    echo "${download_target} does already exist. No need to download."
+  fi
+
+  if [ ! -d $INSTALL_DIR/${final_name} ]; then
+    echo "Extracting ${download_target} to ${INSTALL_DIR}"
+    tar -zxf ${download_target} -C ${INSTALL_DIR}
+
+    echo "Rename to ${final_name}"
+    mv ${INSTALL_DIR}/jdk${archive_version} ${INSTALL_DIR}/${final_name}
+  else
+    echo "${INSTALL_DIR}/${final_name} does already exist. Nothing to install."
+  fi
+
+  echo "Installation of ${final_name} completed."
+
+}
+
+# ###########################################################################
+# Main
+# ###########################################################################
+
+print_env
+init
+download_and_install_oracle_jdk "8" "40" "26"
