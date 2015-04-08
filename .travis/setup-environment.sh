c7fa5d44c2b43e4c93dab3c9680e678d6aec11c9
Stefan Ferstl <st.ferstl@gmail.com>
2015-04-08 23:41:47 +0200

    Add more JDKs

--- .travis/setup-environment.sh
+++ .travis/setup-environment.sh
@@ -121,7 +121,7 @@ if [ -z $REQUIRE_JAVA_RUNTIMES ]; then
   echo "No Java runtimes specified."
 fi
 
-TMP_IFS=$IFS
+tmp_ifs=$IFS
 IFS=","
 for rt in $REQUIRE_JAVA_RUNTIMES; do
   case $rt in
@@ -148,9 +148,84 @@ for rt in $REQUIRE_JAVA_RUNTIMES; do
 
      oracle-jdk-1.7.0_75)
        install_oracle_jdk "7" "75" "13";;
+       
+     oracle-jdk-1.7.0_72)
+       install_oracle_jdk "7" "72" "14";;
+       
+     oracle-jdk-1.7.0_71)
+       install_oracle_jdk "7" "71" "14";;
+       
+     oracle-jdk-1.7.0_67)
+       install_oracle_jdk "7" "67" "01";;
+       
+     oracle-jdk-1.7.0_65)
+       install_oracle_jdk "7" "65" "17";;
+       
+     oracle-jdk-1.7.0_60)
+       install_oracle_jdk "7" "60" "19";;
+       
+     oracle-jdk-1.7.0_55)
+       install_oracle_jdk "7" "55" "13";;
+       
+     oracle-jdk-1.7.0_51)
+       install_oracle_jdk "7" "51" "13";;
+       
+     oracle-jdk-1.7.0_45)
+       install_oracle_jdk "7" "45" "18";;
+       
+     oracle-jdk-1.7.0_40)
+       install_oracle_jdk "7" "40" "43";;
+       
+     oracle-jdk-1.7.0_25)
+       install_oracle_jdk "7" "25" "15";;
+       
+     oracle-jdk-1.7.0_21)
+       install_oracle_jdk "7" "21" "11";;
+       
+     oracle-jdk-1.7.0_17)
+       install_oracle_jdk "7" "17" "02";;
+       
+     oracle-jdk-1.7.0_15)
+       install_oracle_jdk "7" "15" "03";;
+       
+     oracle-jdk-1.7.0_13)
+       install_oracle_jdk "7" "13" "20";;
+       
+     oracle-jdk-1.7.0_11)
+       install_oracle_jdk "7" "11" "21";;
+       
+     oracle-jdk-1.7.0_10)
+       install_oracle_jdk "7" "10" "18";;
+       
+     oracle-jdk-1.7.0_9)
+       install_oracle_jdk "7" "9" "05";;
+       
+     oracle-jdk-1.7.0_7)
+       install_oracle_jdk "7" "7" "10";;
+       
+     oracle-jdk-1.7.0_6)
+       install_oracle_jdk "7" "6" "24";;
+       
+     oracle-jdk-1.7.0_5)
+       install_oracle_jdk "7" "5" "06";;
+       
+     oracle-jdk-1.7.0_4)
+       install_oracle_jdk "7" "4" "20";;
+       
+     oracle-jdk-1.7.0_3)
+       install_oracle_jdk "7" "3" "04";;
+       
+     oracle-jdk-1.7.0_2)
+       install_oracle_jdk "7" "2" "13";;
+       
+     oracle-jdk-1.7.0_1)
+       install_oracle_jdk "7" "1" "08";;
+       
+     oracle-jdk-1.7.0)
+       install_oracle_jdk "7" "" "";;
    esac
 done
-IFS=$TMP_IFS
+IFS=$tmp_ifs
 
 }
 
