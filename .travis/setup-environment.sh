7ac3f885f2e771a1ed814f7aa7f06f10618d8099
Stefan Ferstl <st.ferstl@gmail.com>
2015-04-09 07:52:46 +0200

    Add support for Oracle JRE and Server JRE

--- .travis/setup-environment.sh
+++ .travis/setup-environment.sh
@@ -46,9 +46,10 @@ init() {
 }
 
 install_oracle_jdk() {
-  jdk_major_version=$1
-  jdk_minor_version=$2
-  jdk_build_number=$3
+  java_runtime=$1
+  jdk_major_version=$2
+  jdk_minor_version=$3
+  jdk_build_number=$4
 
   # JDK full version string as it appears in the download URL, e.g. 8u40-b26
   jdk_full_version_string=${jdk_major_version}u${jdk_minor_version}-b${jdk_build_number}
@@ -58,14 +59,18 @@ install_oracle_jdk() {
 
   # JDK version number as it appears within the archive, e.g. 1.8.0_40
   archive_version="1.${jdk_major_version}.0_${jdk_minor_version}"
+  
+  # Folder name in the downloaded archive
+  case $runtime in
+    jdk|server-jre) archive_content="jdk";;
+    jre) archive_content="jre";;
+  esac
 
   # Final name of the downloaded file and of the installation folder
-  final_name="oracle-jdk-${archive_version}"
+  final_name="oracle-${java_runtime}-${archive_version}"
 
-  download_url=http://download.oracle.com/otn-pub/java/jdk/${jdk_full_version_string}/jdk-${jdk_version_string}-linux-x64.tar.gz
-               http://download.oracle.com/otn-pub/java/jdk/8u40-b26                  /server-jre-8u40-linux-x64.tar.gz
-               http://download.oracle.com/otn-pub/java/jdk/8u40-b26                  /jre-8u40-linux-x64.tar.gz
-  download_target=${DOWNLOAD_DIR}/oracle-jdk-${jdk_version_string}.tar.gz
+  download_url=http://download.oracle.com/otn-pub/java/jdk/${jdk_full_version_string}/${java_runtime}-${jdk_version_string}-linux-x64.tar.gz
+  download_target=${DOWNLOAD_DIR}/oracle-${java_runtime}-${jdk_version_string}.tar.gz
 
   if [ ! -d $INSTALL_DIR/${final_name} ]; then
 
@@ -82,7 +87,7 @@ install_oracle_jdk() {
     tar --overwrite -zxf ${download_target} -C ${INSTALL_DIR}
 
     echo "Rename to ${final_name}"
-    mv -f ${INSTALL_DIR}/jdk${archive_version} ${INSTALL_DIR}/${final_name}
+    mv -f ${INSTALL_DIR}/${archive_content}${archive_version} ${INSTALL_DIR}/${final_name}
   else
     echo "${INSTALL_DIR}/${final_name} does already exist. Nothing to install."
   fi
@@ -127,39 +132,39 @@ tmp_ifs=$IFS
 IFS=","
 for rt in $REQUIRE_JAVA_RUNTIMES; do
   case $rt in
-     oracle-jdk-1.8.0_40) install_oracle_jdk "8" "40" "26";;
-     oracle-jdk-1.8.0_31) install_oracle_jdk "8" "31" "13";;
-     oracle-jdk-1.8.0_25) install_oracle_jdk "8" "25" "17";;
-     oracle-jdk-1.8.0_20) install_oracle_jdk "8" "20" "26";;
-     oracle-jdk-1.8.0_11) install_oracle_jdk "8" "11" "12";;
-     oracle-jdk-1.8.0_05) install_oracle_jdk "8" "5" "13";;
-     oracle-jdk-1.8.0) install_oracle_jdk "8" "" "132";;
-     oracle-jdk-1.7.0_75) install_oracle_jdk "7" "75" "13";;
-     oracle-jdk-1.7.0_72) install_oracle_jdk "7" "72" "14";;
-     oracle-jdk-1.7.0_71) install_oracle_jdk "7" "71" "14";;
-     oracle-jdk-1.7.0_67) install_oracle_jdk "7" "67" "01";;
-     oracle-jdk-1.7.0_65) install_oracle_jdk "7" "65" "17";;
-     oracle-jdk-1.7.0_60) install_oracle_jdk "7" "60" "19";;
-     oracle-jdk-1.7.0_55) install_oracle_jdk "7" "55" "13";;
-     oracle-jdk-1.7.0_51) install_oracle_jdk "7" "51" "13";;
-     oracle-jdk-1.7.0_45) install_oracle_jdk "7" "45" "18";;
-     oracle-jdk-1.7.0_40) install_oracle_jdk "7" "40" "43";;
-     oracle-jdk-1.7.0_25) install_oracle_jdk "7" "25" "15";;
-     oracle-jdk-1.7.0_21) install_oracle_jdk "7" "21" "11";;
-     oracle-jdk-1.7.0_17) install_oracle_jdk "7" "17" "02";;
-     oracle-jdk-1.7.0_15) install_oracle_jdk "7" "15" "03";;
-     oracle-jdk-1.7.0_13) install_oracle_jdk "7" "13" "20";;
-     oracle-jdk-1.7.0_11) install_oracle_jdk "7" "11" "21";;
-     oracle-jdk-1.7.0_10) install_oracle_jdk "7" "10" "18";;
-     oracle-jdk-1.7.0_9) install_oracle_jdk "7" "9" "05";;
-     oracle-jdk-1.7.0_7) install_oracle_jdk "7" "7" "10";;
-     oracle-jdk-1.7.0_6) install_oracle_jdk "7" "6" "24";;
-     oracle-jdk-1.7.0_5) install_oracle_jdk "7" "5" "06";;
-     oracle-jdk-1.7.0_4) install_oracle_jdk "7" "4" "20";;
-     oracle-jdk-1.7.0_3) install_oracle_jdk "7" "3" "04";;
-     oracle-jdk-1.7.0_2) install_oracle_jdk "7" "2" "13";;
-     oracle-jdk-1.7.0_1) install_oracle_jdk "7" "1" "08";;
-     oracle-jdk-1.7.0) install_oracle_jdk "7" "" "";;
+     oracle-jdk-1.8.0_40) install_oracle_jdk "jdk"  "8" "40" "26";;
+     oracle-jdk-1.8.0_31) install_oracle_jdk "jdk"  "8" "31" "13";;
+     oracle-jdk-1.8.0_25) install_oracle_jdk "jdk"  "8" "25" "17";;
+     oracle-jdk-1.8.0_20) install_oracle_jdk "jdk"  "8" "20" "26";;
+     oracle-jdk-1.8.0_11) install_oracle_jdk "jdk"  "8" "11" "12";;
+     oracle-jdk-1.8.0_05) install_oracle_jdk "jdk"  "8" "5" "13";;
+     oracle-jdk-1.8.0) install_oracle_jdk "jdk"  "8" "" "132";;
+     oracle-jdk-1.7.0_75) install_oracle_jdk "jdk"  "7" "75" "13";;
+     oracle-jdk-1.7.0_72) install_oracle_jdk "jdk"  "7" "72" "14";;
+     oracle-jdk-1.7.0_71) install_oracle_jdk "jdk"  "7" "71" "14";;
+     oracle-jdk-1.7.0_67) install_oracle_jdk "jdk"  "7" "67" "01";;
+     oracle-jdk-1.7.0_65) install_oracle_jdk "jdk"  "7" "65" "17";;
+     oracle-jdk-1.7.0_60) install_oracle_jdk "jdk"  "7" "60" "19";;
+     oracle-jdk-1.7.0_55) install_oracle_jdk "jdk"  "7" "55" "13";;
+     oracle-jdk-1.7.0_51) install_oracle_jdk "jdk"  "7" "51" "13";;
+     oracle-jdk-1.7.0_45) install_oracle_jdk "jdk"  "7" "45" "18";;
+     oracle-jdk-1.7.0_40) install_oracle_jdk "jdk"  "7" "40" "43";;
+     oracle-jdk-1.7.0_25) install_oracle_jdk "jdk"  "7" "25" "15";;
+     oracle-jdk-1.7.0_21) install_oracle_jdk "jdk"  "7" "21" "11";;
+     oracle-jdk-1.7.0_17) install_oracle_jdk "jdk"  "7" "17" "02";;
+     oracle-jdk-1.7.0_15) install_oracle_jdk "jdk"  "7" "15" "03";;
+     oracle-jdk-1.7.0_13) install_oracle_jdk "jdk"  "7" "13" "20";;
+     oracle-jdk-1.7.0_11) install_oracle_jdk "jdk"  "7" "11" "21";;
+     oracle-jdk-1.7.0_10) install_oracle_jdk "jdk"  "7" "10" "18";;
+     oracle-jdk-1.7.0_9) install_oracle_jdk "jdk"  "7" "9" "05";;
+     oracle-jdk-1.7.0_7) install_oracle_jdk "jdk"  "7" "7" "10";;
+     oracle-jdk-1.7.0_6) install_oracle_jdk "jdk"  "7" "6" "24";;
+     oracle-jdk-1.7.0_5) install_oracle_jdk "jdk"  "7" "5" "06";;
+     oracle-jdk-1.7.0_4) install_oracle_jdk "jdk"  "7" "4" "20";;
+     oracle-jdk-1.7.0_3) install_oracle_jdk "jdk"  "7" "3" "04";;
+     oracle-jdk-1.7.0_2) install_oracle_jdk "jdk"  "7" "2" "13";;
+     oracle-jdk-1.7.0_1) install_oracle_jdk "jdk"  "7" "1" "08";;
+     oracle-jdk-1.7.0) install_oracle_jdk "jdk"  "7" "" "";;
    esac
 done
 IFS=$tmp_ifs
